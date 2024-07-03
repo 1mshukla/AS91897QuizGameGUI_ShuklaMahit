8c0ce3fa243a862057960dc4ce5fec1e6533723e
Mahit Shukla <1mshukla@hbhs.school.nz>
2024-07-03 14:15:25 +1200

    comment and condensed update

--- AS91897QuizGameGUI_ShuklaMahit/Play.cs
+++ AS91897QuizGameGUI_ShuklaMahit/Play.cs
@@ -11,24 +11,34 @@ using System.IO;
 
 namespace AS91897QuizGameGUI_ShuklaMahit
 {
-
+    /// <summary>
+    /// Author: Mahit Shukla
+    /// Last Edited: 03/07/2024
+    /// </summary>
 
     public partial class Play : Form
     {
-        // declare multidimensional array
-        string[,] questionBank = new string[11, 7];
+        //declare costants
+        const int MOVE = 5;
+        const int marioX = 536;
+        const int marioY = 206;
+        const int questionNums = 11;
+        const int multichoiceNums = 7;
+        const int screenIllusion = 40;
+        // declare multidimensional array for quesiton bank
+        string[,] questionBank = new string[questionNums, multichoiceNums];
         //declare lists
         List<string> correctAnswers = new List<string>();
         List<string> incorrectAnswers = new List<string>();
         List<int> usedQuestions = new List<int>();
-        //declare costants
-        const int MOVE = 5;
+        //declare array
+        string[] line = new string[7];
         //declare variables
+        string currentLine;
         int gravity = 4;
         int jumpUp = 20;
         int lineNum = 0;
         int lives = 3;
-        Random sequence = new Random();
         int currentQuestion; 
         bool left;
         bool right;
@@ -37,65 +47,55 @@ namespace AS91897QuizGameGUI_ShuklaMahit
         bool showMessage;
         bool correct;
         bool constantJump = true;
+        //random number generator
+        Random sequence = new Random();
+
 
         public Play()
         {
             InitializeComponent();
-
+            //start player fall as soon as program starts
             fall = true;
             //"K:/2024_SR_12IT1/Programming/AS91897QuizGameGUI_ShuklaMahit/QuizQuestions final.csv"
             //"C:/Users/ANJANA/Documents/Visual Studio 2022/12IT AS91897QuizGameGUI/QuizQuestions final.csv"
-            using (StreamReader things = File.OpenText("C:/Users/ANJANA/Documents/Visual Studio 2022/12IT AS91897QuizGameGUI/QuizQuestions final.csv"))
+            //Takes in the questionbank file and stores it in the questionBank multidimensional array as questions and options
+            using (StreamReader things = File.OpenText("K:/2024_SR_12IT1/Programming/AS91897QuizGameGUI_ShuklaMahit/QuizQuestions final.csv"))
             {
-                string currentLine;
                 while ((currentLine = things.ReadLine()) != null)
                 {
-                    //if (i+1 <= questionBank.GetLength(2))
-                    //{
-                    string[] line = new string[7];
+                    //split different parts of the row by comma and save it as parts of an array, reset the array in each row of file
                     line = currentLine.Split(',');
-
+                    //save the different parts of the file in appropriate rows and columns of questionBank multidimensional array, to be accesed later
                     for (int j = 0; j < line.Length; j++)
                     {
-                        //MessageBox.Show($"{line[j]}");
-                        //string quest = ;
-                        questionBank[lineNum, j] = line[j]; //things.ReadLine().Split(',');
-                                                            //MessageBox.Show(j.ToString());
-                                                            //MessageBox.Show($"{questionBank[i - 1, j]}");
-
+                        questionBank[lineNum, j] = line[j]; 
                     }
+                    //Set the number of row used for each new row started
                     lineNum++;
-                    //}
-                    //else
-                    //{   
-                    //    MessageBox.Show($"break");
-                    //    break;
-
-                    //}
-
                 }
-
             }
-            
+            //show the question as soon as data saved.
             ShowQuestions();
+            //display intial Life count
             textBoxLives.Text = lives.ToString();
-            
         }
 
 
         private void Play_KeyDown_1(object sender, KeyEventArgs e)
         {
-
+            //test if left arrow key pressed then stop initial constant jump for attraction, and move player left
             if (e.KeyCode == Keys.Left)
             {
                 constantJump = false;
                 left = true;
             }
+            //test if right arrow key pressed then stop initial constant jump for attraction, and move player left
             else if (e.KeyCode == Keys.Right)
             {
                 constantJump = false;
                 right = true;
             }
+            //if not already jumping and is not falling and spacebar pressed then and only allow to jump and stop initial constant jump
             if (jump != true && fall != true)
             {
                 if (e.KeyCode == Keys.Space)
@@ -104,6 +104,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                     jump = true;
                 }
             }
+            //if escape key pressed then close program
             if (e.KeyCode == Keys.Escape)
             {
                 Application.Exit();
@@ -113,11 +114,12 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
         private void Play_KeyUp_1(object sender, KeyEventArgs e)
         {
-
+            //if no more pressing left arrow key then stop moving left
             if (e.KeyCode == Keys.Left)
             {
                 left = false;
             }
+            //if no more pressing left arrow key then stop moving left
             else if (e.KeyCode == Keys.Right)
             {
                 right = false;
@@ -127,20 +129,24 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
         private void timerTick_Tick(object sender, EventArgs e)
         {
+            //if intial constant jump is on then keep jumping as soon as landed on a platform
             if (constantJump == true && fall == false)
             {
                 jump = true;
             }
-
+            
             if (jump == true)
             {
                 if (jumpUp > 0)
                 {
+                    //if jump boolean is true and jumping value is greater than 0 then take player picturebox up by jump up value
                     player.Top -= jumpUp;
-                    jumpUp -= 1;
+                    //decrease jumping value by 1 every second to smoothen out jump as max jump distance reached
+                    jumpUp--;
                 }
                 else
                 {
+                    //if max jump height reached and jumping value is 0 or less then reset jump value and stop jump
                     jump = false;
                     jumpUp = 20;
                 }
@@ -148,6 +154,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
             }
 
+            //if player anywhere in air then fall boolean turned on
             if (player.Bottom < this.Height)
             {
                 fall = true;
@@ -158,33 +165,31 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             {
                 if (x is PictureBox && (string)x.Tag == "object")
                 {
-                    if (player.Bounds.IntersectsWith(x.Bounds) && jump == true && player.Top < x.Bottom)
-                    {
-                        //jump = false;
-                        fall = true;
-                    }
+                    //if player touches from the left side of any jumping blocks then sticks to the block and can't go further right into the block
                     if (player.Bounds.IntersectsWith(x.Bounds) && right == true && fall == true && player.Bottom - gravity > x.Top)
                     {
                         right = false;
                     }
+                    //if player touches from the right side of any jumping blocks then sticks to the block and can't go further left into the block
                     if (player.Bounds.IntersectsWith(x.Bounds) && left == true && fall == true && player.Bottom - gravity > x.Top)
                     {
                         left = false;
                     }
+                    //if player lands on top or touches from any side of the jumping block then stops falling
                     if (player.Bounds.IntersectsWith(x.Bounds))
                     {
                         fall = false;
                     }
-                    if (showMessage == false)
-                    {
-                        CheckAnswer();
-
-                    }
                 }
             }
+            //if not already displaying message, check answer and show right or wrong message
+            if (showMessage == false)
+            {
+                CheckAnswer();
 
-            
+            }
 
+            //player collision with left and right walls of the screen
             if (player.Right > this.Width && right == true)
             {
                 right = false;
@@ -195,6 +200,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 left = false;
             }
 
+            //make player move left and right
             if (right == true)
             {
                 player.Left += MOVE;
@@ -207,7 +213,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             //induces fall and stops if reached bottom
             if (fall == true)
             {
-                if (player.Bottom > this.Height - 40)
+                if (player.Bottom > this.Height - screenIllusion)
                 {
                     fall = false;
                     
@@ -229,7 +235,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             {
                 if (questionBank[currentQuestion, 5] == "A")
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = true;
@@ -239,7 +245,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = false;
@@ -252,7 +258,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             {
                 if (questionBank[currentQuestion, 5] == "B")
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = true;
@@ -262,7 +268,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = false;
@@ -275,7 +281,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             {
                 if (questionBank[currentQuestion, 5] == "C")
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = true;
@@ -284,7 +290,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = false;
@@ -297,7 +303,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             {
                 if (questionBank[currentQuestion, 5] == "D")
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = true;
@@ -307,7 +313,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(715, 253);
+                    player.Location = new Point(marioX, marioY);
                     right = false;
                     left = false;
                     correct = false;
@@ -349,10 +355,10 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 if (usedQuestions.Count <=4)
                 {
                     
-                    currentQuestion = sequence.Next(1, 10);
+                    currentQuestion = sequence.Next(1, questionNums - 1);
                     while (usedQuestions.Contains(currentQuestion))
                     {
-                        currentQuestion = sequence.Next(1, 10);
+                        currentQuestion = sequence.Next(1, questionNums - 1);
                     }
                     usedQuestions.Add(currentQuestion);
                     listBox1.Items.Clear();
