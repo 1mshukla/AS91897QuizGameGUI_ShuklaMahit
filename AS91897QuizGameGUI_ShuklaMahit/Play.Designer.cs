08960cdff5e3186dcb279c075b9cca52276dd813
Mahit Shukla <1mshukla@hbhs.school.nz>
2024-07-04 15:10:02 +1200

    FINAL UPDATE. comments updated, file path updated, listbox text bolded.

--- AS91897QuizGameGUI_ShuklaMahit/Play.Designer.cs
+++ AS91897QuizGameGUI_ShuklaMahit/Play.Designer.cs
@@ -63,12 +63,11 @@
             // 
             this.listBox1.Cursor = System.Windows.Forms.Cursors.No;
             this.listBox1.Enabled = false;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(511, 13);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Location = new System.Drawing.Point(358, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(460, 180);
+            this.listBox1.Size = new System.Drawing.Size(404, 147);
             this.listBox1.TabIndex = 22;
             // 
             // timerTick
@@ -81,30 +80,28 @@
             // 
             this.labelLives.AutoSize = true;
             this.labelLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLives.Location = new System.Drawing.Point(16, 25);
-            this.labelLives.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLives.Location = new System.Drawing.Point(12, 20);
             this.labelLives.Name = "labelLives";
-            this.labelLives.Size = new System.Drawing.Size(101, 25);
+            this.labelLives.Size = new System.Drawing.Size(81, 20);
             this.labelLives.TabIndex = 23;
             this.labelLives.Text = "Lives Left:";
             // 
             // textBoxLives
             // 
             this.textBoxLives.Enabled = false;
-            this.textBoxLives.Location = new System.Drawing.Point(132, 25);
-            this.textBoxLives.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLives.Location = new System.Drawing.Point(99, 20);
             this.textBoxLives.Name = "textBoxLives";
             this.textBoxLives.ReadOnly = true;
-            this.textBoxLives.Size = new System.Drawing.Size(31, 22);
+            this.textBoxLives.Size = new System.Drawing.Size(24, 20);
             this.textBoxLives.TabIndex = 24;
             // 
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox5.Location = new System.Drawing.Point(207, 273);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Location = new System.Drawing.Point(155, 222);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(13, 62);
+            this.pictureBox5.Size = new System.Drawing.Size(10, 50);
             this.pictureBox5.TabIndex = 31;
             this.pictureBox5.TabStop = false;
             this.pictureBox5.Tag = "object";
@@ -112,10 +109,9 @@
             // pictureBox4
             // 
             this.pictureBox4.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox4.Location = new System.Drawing.Point(1215, 273);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Location = new System.Drawing.Point(911, 222);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(13, 62);
+            this.pictureBox4.Size = new System.Drawing.Size(10, 50);
             this.pictureBox4.TabIndex = 28;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Tag = "object";
@@ -123,10 +119,9 @@
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox3.Location = new System.Drawing.Point(309, 730);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Location = new System.Drawing.Point(232, 593);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox3.Size = new System.Drawing.Size(44, 38);
             this.pictureBox3.TabIndex = 27;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Tag = "object";
@@ -134,10 +129,9 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox2.Location = new System.Drawing.Point(1076, 730);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(807, 593);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox2.Size = new System.Drawing.Size(44, 38);
             this.pictureBox2.TabIndex = 26;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Tag = "object";
@@ -145,10 +139,9 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Location = new System.Drawing.Point(565, 636);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(424, 517);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(317, 27);
+            this.pictureBox1.Size = new System.Drawing.Size(238, 22);
             this.pictureBox1.TabIndex = 25;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Tag = "object";
@@ -156,10 +149,9 @@
             // block2
             // 
             this.block2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.block2.Location = new System.Drawing.Point(1003, 443);
-            this.block2.Margin = new System.Windows.Forms.Padding(4);
+            this.block2.Location = new System.Drawing.Point(752, 360);
             this.block2.Name = "block2";
-            this.block2.Size = new System.Drawing.Size(79, 62);
+            this.block2.Size = new System.Drawing.Size(59, 50);
             this.block2.TabIndex = 18;
             this.block2.TabStop = false;
             this.block2.Tag = "object";
@@ -168,10 +160,9 @@
             // 
             this.answerC.BackColor = System.Drawing.Color.WhiteSmoke;
             this.answerC.Image = global::AS91897QuizGameGUI_ShuklaMahit.Properties.Resources.large_Letter_c;
-            this.answerC.Location = new System.Drawing.Point(15, 730);
-            this.answerC.Margin = new System.Windows.Forms.Padding(4);
+            this.answerC.Location = new System.Drawing.Point(11, 593);
             this.answerC.Name = "answerC";
-            this.answerC.Size = new System.Drawing.Size(133, 91);
+            this.answerC.Size = new System.Drawing.Size(100, 74);
             this.answerC.TabIndex = 17;
             this.answerC.TabStop = false;
             this.answerC.Tag = "object";
@@ -180,10 +171,9 @@
             // 
             this.answerD.BackColor = System.Drawing.Color.WhiteSmoke;
             this.answerD.Image = global::AS91897QuizGameGUI_ShuklaMahit.Properties.Resources.Letter_D;
-            this.answerD.Location = new System.Drawing.Point(1299, 730);
-            this.answerD.Margin = new System.Windows.Forms.Padding(4);
+            this.answerD.Location = new System.Drawing.Point(974, 593);
             this.answerD.Name = "answerD";
-            this.answerD.Size = new System.Drawing.Size(133, 91);
+            this.answerD.Size = new System.Drawing.Size(100, 74);
             this.answerD.TabIndex = 16;
             this.answerD.TabStop = false;
             this.answerD.Tag = "object";
@@ -194,8 +184,7 @@
             this.answerA.BackgroundImage = global::AS91897QuizGameGUI_ShuklaMahit.Properties.Resources.Letter_A1;
             this.answerA.ErrorImage = null;
             this.answerA.ImageLocation = "centre";
-            this.answerA.Location = new System.Drawing.Point(16, 153);
-            this.answerA.Margin = new System.Windows.Forms.Padding(4);
+            this.answerA.Location = new System.Drawing.Point(12, 124);
             this.answerA.Name = "answerA";
             this.answerA.Size = new System.Drawing.Size(100, 74);
             this.answerA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -207,10 +196,9 @@
             // 
             this.answerB.BackColor = System.Drawing.Color.WhiteSmoke;
             this.answerB.Image = global::AS91897QuizGameGUI_ShuklaMahit.Properties.Resources.letter_B_trial;
-            this.answerB.Location = new System.Drawing.Point(1308, 153);
-            this.answerB.Margin = new System.Windows.Forms.Padding(4);
+            this.answerB.Location = new System.Drawing.Point(981, 124);
             this.answerB.Name = "answerB";
-            this.answerB.Size = new System.Drawing.Size(124, 91);
+            this.answerB.Size = new System.Drawing.Size(93, 74);
             this.answerB.TabIndex = 14;
             this.answerB.TabStop = false;
             this.answerB.Tag = "object";
@@ -219,10 +207,9 @@
             // 
             this.player.BackColor = System.Drawing.Color.WhiteSmoke;
             this.player.Image = global::AS91897QuizGameGUI_ShuklaMahit.Properties.Resources.Smaller_Final_Mario_sprite;
-            this.player.Location = new System.Drawing.Point(715, 253);
-            this.player.Margin = new System.Windows.Forms.Padding(4);
+            this.player.Location = new System.Drawing.Point(536, 206);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(65, 82);
+            this.player.Size = new System.Drawing.Size(49, 67);
             this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.player.TabIndex = 12;
             this.player.TabStop = false;
@@ -230,19 +217,18 @@
             // pictureBox6
             // 
             this.pictureBox6.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox6.Location = new System.Drawing.Point(361, 443);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Location = new System.Drawing.Point(271, 360);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(79, 62);
+            this.pictureBox6.Size = new System.Drawing.Size(59, 50);
             this.pictureBox6.TabIndex = 32;
             this.pictureBox6.TabStop = false;
             this.pictureBox6.Tag = "object";
             // 
             // Play
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1448, 853);
+            this.ClientSize = new System.Drawing.Size(1083, 693);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
@@ -259,7 +245,6 @@
             this.Controls.Add(this.answerB);
             this.Controls.Add(this.player);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Play";
             this.Text = "Play";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Play_KeyDown_1);
--- AS91897QuizGameGUI_ShuklaMahit/Play.cs
+++ AS91897QuizGameGUI_ShuklaMahit/Play.cs
@@ -56,10 +56,8 @@ namespace AS91897QuizGameGUI_ShuklaMahit
             InitializeComponent();
             //start player fall as soon as program starts
             fall = true;
-            //"K:/2024_SR_12IT1/Programming/AS91897QuizGameGUI_ShuklaMahit/QuizQuestions final.csv"
-            //"C:/Users/ANJANA/Documents/Visual Studio 2022/12IT AS91897QuizGameGUI/QuizQuestions final.csv"
             //Takes in the questionbank file and stores it in the questionBank multidimensional array as questions and options
-            using (StreamReader things = File.OpenText("QuizQuestions final.csv"))
+            using (StreamReader things = File.OpenText("../../../../AS91897QuizGameGUI_ShuklaMahit/QuizQuestions final.csv"))
             {
                 while ((currentLine = things.ReadLine()) != null)
                 {
@@ -95,12 +93,11 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 constantJump = false;
                 right = true;
             }
-            //if not already jumping and is not falling and spacebar pressed then and only allow to jump and stop initial constant jump
+            //if not already jumping and is not falling and spacebar pressed then and only allow to jump
             if (jump != true && fall != true)
             {
                 if (e.KeyCode == Keys.Space)
                 {
-                    constantJump = false;
                     jump = true;
                 }
             }
@@ -208,7 +205,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 player.Left -= MOVE;
             }
 
-            //induces fall and stops if reached bottom
+            //makes player fall from air and stops if reached bottom
             if (fall == true)
             {
                 if (player.Bottom > this.Height - SCREEN_ILLUSION)
@@ -227,11 +224,15 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
         }
 
+        /// <summary>
+        /// Checks if the answer from player is true or false
+        /// Resets player position and displays the correct answer and next question
+        /// </summary>
         private void CheckAnswer()
         {
-            if (player.Bounds.IntersectsWith(answerA.Bounds))
+            if (player.Bounds.IntersectsWith(answerA.Bounds)) //checks if touching answer block A
             {
-                if (questionBank[currentQuestion, 5] == "A")
+                if (questionBank[currentQuestion, 5] == "A") //if correct answer is also A then they are correct
                 {
                     player.Location = new Point(MARIOX, MARIOY);
                     right = false;
@@ -243,7 +244,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); //if correct answer is not A then they are incorrect
                     right = false;
                     left = false;
                     correct = false;
@@ -252,9 +253,9 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
                 }
             }
-            if (player.Bounds.IntersectsWith(answerB.Bounds))
+            if (player.Bounds.IntersectsWith(answerB.Bounds)) //checks if touching answer block B
             {
-                if (questionBank[currentQuestion, 5] == "B")
+                if (questionBank[currentQuestion, 5] == "B") //if correct answer is also B then they are correct
                 {
                     player.Location = new Point(MARIOX, MARIOY);
                     right = false;
@@ -266,7 +267,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); //if correct answer is not B then they are incorrect
                     right = false;
                     left = false;
                     correct = false;
@@ -275,11 +276,11 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
                 }
             }
-            if (player.Bounds.IntersectsWith(answerC.Bounds))
+            if (player.Bounds.IntersectsWith(answerC.Bounds)) //checks if touching answer block C
             {
-                if (questionBank[currentQuestion, 5] == "C")
+                if (questionBank[currentQuestion, 5] == "C") //if correct answer is also C then they are correct
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); 
                     right = false;
                     left = false;
                     correct = true;
@@ -288,7 +289,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); //if correct answer is not C then they are incorrect
                     right = false;
                     left = false;
                     correct = false;
@@ -297,11 +298,11 @@ namespace AS91897QuizGameGUI_ShuklaMahit
 
                 }
             }
-            if (player.Bounds.IntersectsWith(answerD.Bounds))
+            if (player.Bounds.IntersectsWith(answerD.Bounds)) //checks if touching answer block D
             {
                 if (questionBank[currentQuestion, 5] == "D")
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); //if correct answer is also D then they are correct
                     right = false;
                     left = false;
                     correct = true;
@@ -311,7 +312,7 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
                 else
                 {
-                    player.Location = new Point(MARIOX, MARIOY);
+                    player.Location = new Point(MARIOX, MARIOY); //if correct answer is not D then they are incorrect
                     right = false;
                     left = false;
                     correct = false;
@@ -321,9 +322,16 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 }
             }
         }
+
+        /// <summary>
+        /// Displays if the answer is correct or not and what the actual correct answer is
+        /// Goes to ShowQuestions to display the next question
+        /// Prevents from displaying the messagebox again and again as timer ticks
+        /// Deducts a life if answer is wrong
+        /// </summary>
         private void DisplayAnswer()
         {
-            if (correct == true)
+            if (correct == true) //if answered correctly then says it is correct adds the record to list of correct answers
             {
                 MessageBox.Show($"CORRECT. THE ANSWER IS: {questionBank[currentQuestion, 6]}");
                 
@@ -333,11 +341,11 @@ namespace AS91897QuizGameGUI_ShuklaMahit
                 ShowQuestions();
 
             }
-            else
+            else //answered incorrectly then says incorrect and what the actual answer is adds the record to list of incorrect answer
             {
                 MessageBox.Show($"INCORRECT. THE ANSWER IS: {questionBank[currentQuestion, 6]}");
                 
-                lives--;
+                lives--; //takes away a life if answer is wrong
                 textBoxLives.Text = lives.ToString();
                 showMessage = false;
                 incorrectAnswers.Add(questionBank[currentQuestion, 0]);
@@ -348,62 +356,67 @@ namespace AS91897QuizGameGUI_ShuklaMahit
         private void ShowQuestions()
         {
             
-            if (lives > 0)
+            if (lives > 0) //if still has lives then displays next random question
             {
-                if (usedQuestions.Count <=4)
+                if (usedQuestions.Count <=4) //if not already displayed 5 questions and game not over then displays the next random question
                 {
-                    
+                    //picks random number not already chosen before and picks that number of question to display
                     currentQuestion = sequence.Next(1, QUESTION_NUMS - 1);
                     while (usedQuestions.Contains(currentQuestion))
                     {
                         currentQuestion = sequence.Next(1, QUESTION_NUMS - 1);
                     }
+                    //adds the current question to list of used questions to prevent it from being picked again
                     usedQuestions.Add(currentQuestion);
-                    listBox1.Items.Clear();
+                    listBox1.Items.Clear(); // clear last question
+                    //display question
                     listBox1.Items.Add($"Question: {questionBank[currentQuestion, 0]}");
+                    //display the 4 options for multichoice
                     listBox1.Items.Add($"Option A is {questionBank[currentQuestion, 1]}");
                     listBox1.Items.Add($"Option B is {questionBank[currentQuestion, 2]}");
                     listBox1.Items.Add($"Option C is {questionBank[currentQuestion, 3]}");
                     listBox1.Items.Add($"Option D is {questionBank[currentQuestion, 4]}");
                 }
-                else
+                else //otherwise if game is over then display message box showing it is over and the scores are recorded
                 {
-                    MessageBox.Show("THANK YOU FOR PLAYING, QUIZ SCORES RECORDED IN NOTEPAD IN FOLDER!");
-                    using (StreamWriter summary = new StreamWriter("Answer Summary.txt"))
+                    
+                    using (StreamWriter summary = new StreamWriter("Answer Summary.txt")) //output summary to a text file
                     {
                         summary.WriteLine("You answered correctly:");
-                        foreach (string answer in correctAnswers)
+                        foreach (string answer in correctAnswers) //in the file display all the correct answers
                         {
                             summary.WriteLine(answer);
                         }
                         summary.WriteLine("You answered incorrectly:");
-                        foreach (string wrong in incorrectAnswers)
+                        foreach (string wrong in incorrectAnswers) //in the file display all the incorrect answers
                         {
                             summary.WriteLine(wrong);
                         }
                     
                     }
-                    Application.Exit();
+                    MessageBox.Show("THANK YOU FOR PLAYING, QUIZ SCORES RECORDED IN NOTEPAD IN DEBUG FOLDER!");
+                    Application.Exit(); //close program
                 }
             }
-            else
+            else //otherwise if all lives lost then display message box showing it is over and the scores are recorded
             {
-                MessageBox.Show("Sorry, out of lives. GAME OVER!");
-                using (StreamWriter summary = new StreamWriter("Answer Summary.txt"))
+                
+                using (StreamWriter summary = new StreamWriter("Answer Summary.txt")) //output summary to a text file
                 {
                     summary.WriteLine("You answered correctly:");
-                    foreach (string answer in correctAnswers)
+                    foreach (string answer in correctAnswers) //in the file display all the correct answers
                     {
                         summary.WriteLine(answer);
                     }
                     summary.WriteLine("You answered incorrectly:");
-                    foreach (string wrong in incorrectAnswers)
+                    foreach (string wrong in incorrectAnswers) //in the file display all the incorrect answers
                     {
                         summary.WriteLine(wrong);
                     }
                     
                 }
-                Application.Exit();
+                MessageBox.Show("Sorry, out of lives. GAME OVER! Quiz Scores recorded in notepad in debug folder.");
+                Application.Exit(); //close program
 
             }
 
